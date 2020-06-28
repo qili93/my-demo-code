57727e3942a4eb69fb81ed7be980637704d9227f
qili93 <selmalq@gmail.com>
2020-06-28 12:58:31 +0000

    update demo

--- bias_add_demo/model_run/bias_add_run.cc
+++ bias_add_demo/model_run/bias_add_run.cc
@@ -3,6 +3,10 @@
 using namespace std;
 bool g_isDevice = false;
 
+#define INFO_LOG(fmt, args...) fprintf(stdout, "[INFO]  " fmt "\n", ##args)
+#define WARN_LOG(fmt, args...) fprintf(stdout, "[WARN]  " fmt "\n", ##args)
+#define ERROR_LOG(fmt, args...) fprintf(stdout, "[ERROR] " fmt "\n", ##args)
+
 typedef enum Result {
     SUCCESS = 0,
     FAILED = 1
