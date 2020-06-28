d3c740bc6b01e124cb14f654c71b95b0c3510127
qili93 <selmalq@gmail.com>
2020-06-28 11:41:31 +0000

    update ascend demo

---
+++ bias_add_demo/model_build/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cur_dir=$(pwd)
+
+build_dir=$cur_dir/build
+rm -rf $build_dir
+mkdir -p $build_dir
+cd $build_dir
+
+cmake -DASCEND_PATH=/workspace/Ascend310 \
+      -DCMAKE_VERBOSE_MAKEFILE=ON \
+      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+      -DCMAKE_BUILD_TYPE=Release \
+      ..
+make
+
+cd -
+echo "ls -l $build_dir"
+ls -l $build_dir
---
+++ bias_add_demo/model_build/build/CMakeFiles/bias_add_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bias_add_demo.dir/bias_add_demo.cc.o"
+  "bias_add_demo.pdb"
+  "bias_add_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bias_add_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
