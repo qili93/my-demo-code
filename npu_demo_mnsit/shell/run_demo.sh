c96d41614b6a1c8fd3a87c9967a76919c1a69550
qili93 <selmalq@gmail.com>
2020-07-07 20:53:34 +0800

    update scripts

--- npu_demo_mnsit/shell/run_demo.sh
+++ npu_demo_mnsit/shell/run_demo.sh
@@ -1,146 +1,56 @@
 #!/bin/bash
-# set -ex
-#######################################################################
-# global variables, you can change them according to your requirements
-#######################################################################
-# absolute path current
+cur_dir=$(pwd)
+
 function readlinkf() {
     perl -MCwd -e 'print Cwd::abs_path shift' "$1";
 }
-# arm64-v8a or armeabi-v7a
-# ANDROID_ABI=arm64-v8a
-ANDROID_ABI=armeabi-v7a
-# arm64-v8a => 23 or armeabi-v7a => 24
-ANDROID_NATIVE_API_LEVEL=android-24 # armeabi-v7a
-# ANDROID_NATIVE_API_LEVEL=android-23 # arm64-v8a 
-# default with log
-PADDLE_LITE_DIR=$(readlinkf ../../../libs/npu/${ANDROID_ABI}-log)
-#######################################################################
-# local variables, do not change them
-#######################################################################
+
+#######################################
+# Local Settings: please change accrodingly
+#######################################
+# ANDROID_NDK=/opt/android-ndk-r17c # For paddlelite docker
+ANDROID_ABI=armeabi-v7a # arm64-v8a
+ANDROID_NATIVE_API_LEVEL=android-23
+if [ $ANDROID_ABI == "armeabi-v7a" ]; then
+    ANDROID_NATIVE_API_LEVEL=android-24
+fi
+
+# paddle lite dir
+PADDLE_LITE_DIR=$(readlinkf ../../npu_lite_libs/armeabi-v7a-log)
+
+#######################################
+# Model variables, do not change them
+#######################################
 # build target
 TARGET_EXE=mnist_demo
 # model name
 MODEL_NAME=mnist_model
 # model dir
-MODEL_DIR="../assets/models"
-# model path
-MODEL_PATH="$MODEL_DIR/${MODEL_NAME}"
+MODEL_DIR=$(readlinkf ../assets/models)
 # workspace
 WORK_SPACE=/data/local/tmp
-#######################################################################
-# pring usage
-#######################################################################
-function print_usage {
-    set +x
-    echo -e "\nUSAGE:"
-    echo
-    echo "----------------------------------------"
-    echo -e "compile tiny publish so lib:"
-    echo -e "   ./run_demo.sh --arm_abi=<abi> --with_log=<ON|OFF> tiny_demo"
-    echo
-    echo -e "compile full publish so lib:"
-    echo -e "   ./run_demo.sh --arm_abi=<abi> --with_log=<ON|OFF> full_demo"
-    echo
-    echo -e "optional argument:"
-    echo -e "--with_log: (OFF|ON); controls whether to print log information, default is ON"
-    echo
-    echo -e "argument choices:"
-    echo -e "--arm_abi:\t armv8|armv7; default is armv7"
-    echo
-    echo -e "tasks:"
-    echo -e "tiny_demo: demo with tiny publish library."
-    echo -e "tiny_demo: demo with full publish library."
-    echo "----------------------------------------"
-    echo
-}
 
-#######################################################################
-# compiling functions
-#######################################################################
-function run_demo {
-    local abi=$1
-    local log=$2
-    local pub=$3
+#######################################
+# Running commands, do not change
+#######################################
+# push to device work space
+adb shell   "rm -r ${WORK_SPACE}/*"
+adb push   ${PADDLE_LITE_DIR}/lib/.     ${WORK_SPACE}
+adb push   ${MODEL_DIR}/.                   ${WORK_SPACE}
+adb push   build/${TARGET_EXE}           ${WORK_SPACE}
+adb shell   chmod +x "${WORK_SPACE}/${TARGET_EXE}"
+# define exe commands
+EXE_SHELL="cd ${WORK_SPACE}; "
+EXE_SHELL+="export GLOG_v=5;"
+EXE_SHELL+="LD_LIBRARY_PATH=. ./${TARGET_EXE} ./${MODEL_NAME}"
+echo ${EXE_SHELL}
+# run
+adb shell ${EXE_SHELL}
+adb shell ls -l ${WORK_SPACE}
 
-    if [ ${pub} == "tiny" ]; then
-        MODEL_PATH=${MODEL_PATH}.nb
-    fi
-    echo "Building api: USE_FULL_API=<${USE_FULL_API}>"
+# pull optimized model
+adb pull ${WORK_SPACE}/${MODEL_NAME}.nb ${MODEL_DIR}
 
-    if [ ${abi} == "armv8" ]; then
-        ANDROID_ABI=arm64-v8a
-        ANDROID_NATIVE_API_LEVEL=android-23
-    else
-        ANDROID_ABI=armeabi-v7a
-        ANDROID_NATIVE_API_LEVEL=android-24
-    fi
-    echo "Running target: ANDROID_ABI=<${ANDROID_ABI}> ANDROID_NATIVE_API_LEVEL=<${ANDROID_NATIVE_API_LEVEL}>"
-
-    if [ ${log} == "ON" ]; then
-        PADDLE_LITE_DIR_LIB=$(readlinkf ../../libs/npu/${ANDROID_ABI}-log/lib)
-    else
-        PADDLE_LITE_DIR_LIB=$(readlinkf ../../libs/npu/${ANDROID_ABI}-nolog/lib)
-    fi
-    echo "Running with library: PADDLE_LITE_DIR_LIB=<${PADDLE_LITE_DIR_LIB}>"
-
-    # push to device work space
-    adb shell   "rm -r ${WORK_SPACE}/*"
-    adb push   ${PADDLE_LITE_DIR_LIB}/.     ${WORK_SPACE}
-    adb push   ${MODEL_PATH}                  ${WORK_SPACE}
-    adb push   build/${TARGET_EXE}           ${WORK_SPACE}
-    adb shell   chmod +x "${WORK_SPACE}/${TARGET_EXE}"
-    # define exe commands
-    EXE_SHELL="cd ${WORK_SPACE}; "
-    EXE_SHELL+="export GLOG_v=5;"
-    EXE_SHELL+="LD_LIBRARY_PATH=. ./${TARGET_EXE} ./${MODEL_NAME}"
-    echo ${EXE_SHELL}
-    # run
-    adb shell ${EXE_SHELL}
-    adb shell ls -l ${WORK_SPACE}
-
-    # pull optimized model
-    adb pull ${WORK_SPACE}/${MODEL_NAME}.nb ${MODEL_DIR}
-
-    # list models files
-    echo "ls -l ${MODEL_DIR}"
-    ls -l ${MODEL_DIR}
-}
-#######################################################################
-# main functions
-#######################################################################
-function main {
-    if [ -z "$1" ]; then
-        print_usage
-        exit -1
-    fi
-
-    # Parse command line.
-    for i in "$@"; do
-        case $i in
-            --arm_abi=*)
-                ARM_ABI="${i#*=}"
-                shift
-                ;;
-            --with_log=*)
-                WITH_LOG="${i#*=}"
-                shift
-                ;;
-            tiny_demo)
-                run_demo $ARM_ABI $WITH_LOG tiny
-                shift
-                ;;
-            full_demo)
-                run_demo $ARM_ABI $WITH_LOG full
-                shift
-                ;;
-            *)
-                # unknown option
-                print_usage
-                exit 1
-                ;;
-        esac
-    done
-}
-
-main $@
+# list models files
+echo "ls -l ${MODEL_DIR}"
+ ls -l ${MODEL_DIR}
