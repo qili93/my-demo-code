db5c54156797c5dc1bfa612f5c88b61f969466be
qili93 <qili93@qq.com>
2020-08-13 20:36:30 +0800

    update demo

--- ascend_demo/ascend_ernie_test/shell/run_demo.sh
+++ ascend_demo/ascend_ernie_test/shell/run_demo.sh
@@ -44,6 +44,7 @@ echo "export SOC_VERSION=$SOC_VERSION"
 # set paddle-lite environment
 BASE_REPO_PATH=/workspace/Github-qili93/Paddle-Lite
 # BASE_REPO_PATH=/workspace/temp_repo/Paddle-Lite
+
 PADDLE_LITE_DIR=$BASE_REPO_PATH/build.lite.huawei_ascend_npu/inference_lite_lib
 export LD_LIBRARY_PATH=${PADDLE_LITE_DIR}/cxx/lib:${PADDLE_LITE_DIR}/third_party/mklml/lib:$LD_LIBRARY_PATH
 
