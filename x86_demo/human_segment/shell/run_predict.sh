f02c642f9c82f426563b57d0a583e254e49be752
qili93 <selmalq@gmail.com>
2020-09-15 02:12:17 +0000

    add x86 demo

---
+++ x86_demo/human_segment/shell/run_predict.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+cur_dir=$(pwd)
+
+function readlinkf() {
+    perl -MCwd -e 'print Cwd::abs_path shift' "$1";
+}
+
+#######################################
+# Local Settings: paddle-lite envs
+#######################################
+
+# set paddle-lite environment
+BASE_REPO_PATH=/workspace/Github-qili93/Paddle-Lite
+PADDLE_LITE_DIR=$BASE_REPO_PATH/build.lite.x86/inference_lite_lib
+export LD_LIBRARY_PATH=${PADDLE_LITE_DIR}/cxx/lib:${PADDLE_LITE_DIR}/third_party/mklml/lib:$LD_LIBRARY_PATH
+
+# set model dir
+MODEL_DIR=$(readlinkf ../assets/models)
+MODEL_NAME=align150-customized-pa-v3_ar46.model.float32-1.0.2.1
+# MODEL_NAME=mobilenet_v2
+MODEL_TYPE=1 # 0 uncombined; 1 combined paddle fluid model
+
+# run demo
+export GLOG_v=5
+./build/human_seg_demo $MODEL_DIR/$MODEL_NAME $MODEL_TYPE run
