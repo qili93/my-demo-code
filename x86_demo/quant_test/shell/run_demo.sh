f5320aa4dedb7e29bd0bfcd8aceaf16e36957102
qili93 <qili93@qq.com>
2020-11-05 12:18:38 +0000

    update

--- x86_demo/quant_test/shell/run_demo.sh
+++ x86_demo/quant_test/shell/run_demo.sh
@@ -10,24 +10,19 @@ function readlinkf() {
 #######################################
 
 # paddle repo dir
-if [[ "$OSTYPE" == "darwin"*  ]]; then # MACOS
-  BASE_REPO_PATH=/Users/liqi27/Documents/Github-qili93/Paddle-Lite
-  PADDLE_LITE_DIR=$BASE_REPO_PATH/build.lite.x86/inference_lite_lib
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then # Linux
-  BASE_REPO_PATH=/workspace/Github-origin/Paddle-Lite
-  PADDLE_LITE_DIR=$BASE_REPO_PATH/build.lite.x86/inference_lite_lib
-fi
+BASE_REPO_PATH=$(readlinkf ../../../../Paddle-Lite)
+BUILD_DIR_NAME=build-dev-debug
+PADDLE_LITE_DIR=${BASE_REPO_PATH}/${BUILD_DIR_NAME}/build.lite.x86/inference_lite_lib
 export LD_LIBRARY_PATH=${PADDLE_LITE_DIR}/cxx/lib:${PADDLE_LITE_DIR}/third_party/mklml/lib:$LD_LIBRARY_PATH
 
-# for local lib
+# # local sync lib dir
 # PADDLE_LITE_DIR=$(readlinkf ../../x86_lite_libs)
 # export LD_LIBRARY_PATH=${PADDLE_LITE_DIR}/lib:$LD_LIBRARY_PATH
 
 # set model dir
-MODEL_DIR=$(readlinkf ../assets/models)
-MODEL_TYPE=1 # 0 uncombined; 1 combined paddle fluid model
+MODEL_DIR=$(readlinkf ../python)
 MODEL_NAME=mnist_quant
 
 # run demo
 export GLOG_v=5
-./build/mnist_quant_demo $MODEL_DIR $MODEL_NAME $MODEL_TYPE
+./build/quant_test $MODEL_DIR/$MODEL_NAME
