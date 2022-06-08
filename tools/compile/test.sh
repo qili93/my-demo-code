07bbc62a5ed999f8d8422a134bd36fe4bf3d90d8
qili93 <qili93@qq.com>
2022-06-08 17:47:50 +0800

    remove

--- tools/compile/test.sh
+++
@@ -1,9 +0,0 @@
-arch=$(uname -i)
-if [[ $arch == x86_64* ]]; then
-    echo "X64 Architecture"
-elif [[ $arch == i*86 ]]; then
-    echo "X32 Architecture"
-elif  [[ $arch == arm* ]]; then
-    echo "ARM Architecture"
-fi
-
