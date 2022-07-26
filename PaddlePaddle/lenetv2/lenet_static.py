4e74c566cdbe9df3e2a32b8b265fc0309d21de77
qili93 <qili93@qq.com>
2022-07-26 12:55:51 +0800

    update commit

--- PaddlePaddle/lenetv2/lenet_static.py
+++ PaddlePaddle/lenetv2/lenet_static.py
@@ -1,3 +1,17 @@
+#   Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import numpy as np
 
@@ -5,7 +19,6 @@ import paddle
 import paddle.nn as nn
 import paddle.static as static
 from paddle.vision.transforms import Compose, Normalize
-from paddle.utils.cpp_extension import load
 
 os.environ['CPU_NUM'] = '4'
 EPOCH_NUM = 1
