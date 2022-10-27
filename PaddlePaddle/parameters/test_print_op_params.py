b962e00c3d5cad05f1388dde320955a7a7eece6e
qili93 <qili93@qq.com>
2022-10-27 09:22:40 +0000

    add

--- PaddlePaddle/parameters/test_print_op_params.py
+++ PaddlePaddle/parameters/test_print_op_params.py
@@ -21,3 +21,7 @@ for name, param in batch_norm_2d.named_parameters():
 # _variance Parameter containing:
 # Tensor(shape=[6], dtype=float32, place=Place(cpu), stop_gradient=True,
 #        [1., 1., 1., 1., 1., 1.])
+
+conv_2d = nn.Conv2D(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=0)
+for name, param in conv_2d.named_parameters():
+    print(name, param)
