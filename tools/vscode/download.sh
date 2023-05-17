ddc4ca9be09b3f6d0f490af676188b57a4b01a31
qili93 <qili93@qq.com>
2023-05-17 16:33:29 +0800

    add npu ops

--- tools/vscode/download.sh
+++ tools/vscode/download.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-arch=$(uname -i)
-if [[ $arch == x86_64* ]]; then
-    wget https://github.com/microsoft/vscode-cpptools/releases/download/1.1.2/cpptools-linux.vsix
-elif  [[ $arch == aarch64* ]]; then
-    wget https://github.com/microsoft/vscode-cpptools/releases/download/1.1.2/cpptools-linux-aarch64.vsix
-fi
-
+wget https://github.com/microsoft/vscode-cpptools/releases/download/v1.14.4/cpptools-linux-$(uname -m).vsix
 wget https://github.com/gitkraken/vscode-gitlens/releases/download/v12.2.2/gitlens-12.2.2.vsix
 wget https://github.com/microsoft/vscode-python/releases/download/2021.1.502429796/ms-python-release.vsix
 wget -O twxs.cmake-0.0.17.vsix https://marketplace.visualstudio.com/_apis/public/gallery/publishers/twxs/vsextensions/cmake/0.0.17/vspackage
